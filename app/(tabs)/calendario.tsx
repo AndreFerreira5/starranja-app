0f97f8793ebf4dc8edeaf5db4c61896dc3811f99
Nuno <onunorodrigues@gmail.com>
2025-11-27 13:49:19 +0000

    Create TabBar and base page structure

---
+++ app/(tabs)/calendario.tsx
@@ -0,0 +1,10 @@
+import { YStack, H1, Text } from 'tamagui';
+
+export default function CalendarioPage() {
+  return (
+    <YStack flex={1} padding="$4" gap="$4">
+      <H1>Calendário</H1>
+      <Text>Em desenvolvimento...</Text>
+    </YStack>
+  );
+}
